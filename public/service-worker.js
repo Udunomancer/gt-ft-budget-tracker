c9149efb635780edc22ba51502f44c530a2261c1
Kyle Chriscoe <kyle717@gmail.com>
2021-02-11 18:17:35 -0500

    ran Prettier on service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,54 +42,54 @@ self.addEventListener("install", function (e) {
 
 // activate
 self.addEventListener("activate", function (e) {
-    e.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(
-                keyList.map((key) => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
 
-    self.clients.claim();
+  self.clients.claim();
 });
 
 // fetch
 self.addEventListener("fetch", function (e) {
-    if (e.request.url.includes("/api/")) {
-        e.respondWith(
-            caches
-            .open(DATA_CACHE_NAME)
-            .then((cache) => {
-                return fetch(e.request)
-                .then((response) => {
-                    // If the response was good, clone it and store it in the cache.
-                    if (response.status === 200) {
-                        cache.put(e.request.url, response.clone());
-                    }
-
-                    return response;
-                })
-                .catch((err) => {
-                    // Network request failed, try to get it from the cache.
-                    return cache.match(e.request);
-                });
-            })
-            .catch((err) => console.log(err))
-        );
-
-        return;
-    }
-
+  if (e.request.url.includes("/api/")) {
     e.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(e.request).then((response) => {
-                return response || fetch(e.request);
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(e.request);
             });
         })
+        .catch((err) => console.log(err))
     );
+
+    return;
+  }
+
+  e.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      });
+    })
+  );
 });
